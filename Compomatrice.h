2f404d18b9d3d2ff524e59441793461bed03a03e
Aymeric <99363574+aaetuo@users.noreply.github.com>
2024-03-28 09:16:54 +0100

    Create Compomatrice.h

---
+++ Compomatrice.h
@@ -0,0 +1,158 @@
+void deplacer_snoopy(char matrice[12][20]) { //On affiche le plateau comme s'il était avec des coordonnées permetant des deplacements cibles
+
+    int i, j;
+    char mat[12][20];
+    // Recherche de la position de snoopy
+    for (i = 0; i < 12; i++) {
+        for (j = 0; j < 20; j++) {
+            if (mat[i][j] == 8) {
+                x = i;
+                y = j;
+                break;
+            }
+        }
+        if (mat[i][j] == 8) {
+            break;
+        }
+    }
+    while (1) {
+
+        // Affichage de la matrice
+        for (i = 0; i < 12; i++) {
+
+            for (j = 0; j < 20; j++) {
+
+                //Plan de travail
+                if (mat[i][j] == 0){
+                }
+
+                //Plaque de cuisson
+
+                if (mat[i][j] == 1){
+                }
+
+                //Friteuse
+
+                if (mat[i][j] == 2){
+                }
+
+                //boite à pain à burger
+
+                if (mat[i][j] == 3){
+                }
+
+                //boite à steak brut
+
+                if (mat[i][j] == 4){
+                }
+
+                //boite à patate brute
+
+                if (mat[i][j] == 5){
+                }
+
+                //boite à salade brute
+
+                if (mat[i][j] == 6){
+                }
+
+                //boite à oignons brut
+
+                if (mat[i][j] == 7){
+                }
+
+                //boite à tomate brute
+
+                if (mat[i][j] == 9){
+                }
+
+                //Espace de découpe
+                if (mat[i][j] == 0){
+                }
+
+                //Coffre à ingrédients
+                if (mat[i][j] == 0){
+                }
+
+              //Distributeur d'assiettes
+                if (mat[i][j] == 0){
+                }
+
+              //Poubelle
+                if (mat[i][j] == 0){
+                }
+
+              //Sortie de cuisine
+                if (mat[i][j] == 0){
+                }
+
+                //Boite de sauce Barbecue
+                if (mat[i][j] == 0){
+                }
+
+                //boite à spaghetti crue
+                if (mat[i][j] == 0){
+                }
+
+                //boite à bolognaise
+                if (mat[i][j] == 0){
+                }
+
+                //boite à carbonara
+                if (mat[i][j] == 0){
+                }
+
+                //boite à poisson cru
+                if (mat[i][j] == 0){
+                }
+
+                //boite à pepperoni
+                if (mat[i][j] == 0){
+                }
+
+                //boite à fromage
+                if (mat[i][j] == 0){
+                }
+                
+                //boite à nems
+                if (mat[i][j] == 0){
+                }
+              
+                //boite à riz
+                if (mat[i][j] == 0){
+                }
+              
+                //boite à crevette
+                if (mat[i][j] == 0){
+                }
+              
+                //boite à sushis
+                if (mat[i][j] == 0){
+                }
+              
+                //Four
+                if (mat[i][j] == 0){
+                }
+              
+                //Distributeur de bucket
+                if (mat[i][j] == 0){
+                }
+
+                //Boite à poulet cru
+                if (mat[i][j] == 0){
+                }
+
+                //boite à pate à pizza
+                if (mat[i][j] == 0){
+                }
+
+                //boite à fromage
+                if (mat[i][j] == 0){
+                }
+
+              
+            }
+            printf("\n");
+        }
+}
+}
